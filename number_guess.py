9751cd7194190aa4d155cce3759d0d86bb27c50e
RyanLanz123 <ryanlanz6@gmail.com>
2024-11-07 13:09:02 +0000

    add if/else/while

--- number_guess.py
+++ number_guess.py
@@ -1,4 +1,31 @@
 import random 
 
-r = random.randrange(-5, 11)
-print(r)
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print('Please type number larger than 0 next time')
+        quit()
+else:
+    print('Print type a number next time')
+    quit()
+
+random_number = random.randint(0, top_of_range)
+
+
+while True:
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+
+    else:
+        print('Print type a number next time')
+        continue
+
+    if user_guess == random_number:
+        print("Correct")
+        break
+    else:
+        print("Incorrect")
